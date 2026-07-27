2ad67ffd8cdc7cca0f7a78e9fdfcfd89cbc6efa9
andrew <92565929+Paracosms@users.noreply.github.com>
2026-07-26 17:35:26 -0700

    update to 26.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,9 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
-    testCompileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
-    testRuntimeOnly("io.papermc.paper:paper-api:26.1.2.build.+")
+    compileOnly("io.papermc.paper:paper-api:26.2.build.+")
+    testCompileOnly("io.papermc.paper:paper-api:26.2.build.+")
+    testRuntimeOnly("io.papermc.paper:paper-api:26.2.build.+")
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
